d35b4af5b54216fe3c9c9376f2f6a90ba5a5d8d2
Pilmo Kang <ivorycirrus@gmail.com>
2018-09-18 12:12:21 +0900

    Dlmatrix : add function

--- math/matrix.js
+++ math/matrix.js
@@ -20,10 +20,10 @@ const DlMatrix = function(ctxRoot){
 		} else {
 			if(_size) return _size;
 		}
-	}
+	};
 	
 
-	/* Matrix addition */	
+	/* Matrix addition */
 	let _matrix_add = function(arr1, arr2){
 		if(!Array.isArray(arr1) || !Array.isArray(arr2)) throw "MatrixException : parameters are not array.";
 		else if(arr1.length != arr2.length) throw "MatrixException : Size of arrays are different.";
@@ -36,6 +36,75 @@ const DlMatrix = function(ctxRoot){
 			}
 			return result;
 		}
+	};
+
+	/* Scalar addition */
+	let _scalar_add = function(arr1, num){
+		if(!Array.isArray(arr1)) throw "MatrixException : first parameter is not array.";
+		else if(arr1.length == 0) return null;
+		else {
+			let result = [];
+			for(let i = 0 ; i < arr1.length ; i++) {
+				if(Array.isArray(arr1[i])) result[i] = _scalar_add(arr1[i], num);
+				else result[i] = arr1[i] + num;
+			}
+			return result;
+		}
+	};
+
+	/* Matrix multiply */
+	let _matrix_mul = function(arr1, arr2){
+		if(!Array.isArray(arr1) || !Array.isArray(arr2)) throw "MatrixException : parameters are not array.";
+
+		const s1 = _matrix_shape(arr1, []);
+		const s2 = _matrix_shape(arr2, []);
+		if(s1.length != 2 || s2.length != 2) throw "MatrixException : input arrays are not 2d array.";
+		else if(s1[1] != s2[0]) throw "MatrixException : array shapes are mismatch.";
+		else if(s1[0] == 0 || s2[1] == 0) throw "MatrixException : cannot multiple zseo-size array.";
+
+		const rows = s1[0], cols = s2[1], middle = s1[1];
+		let result = [];
+		for(let i = 0 ; i < rows ; i++) {
+			let row = [];
+			for(let j = 0 ; j < cols ; j++) {
+				let cell = 0;
+				for(let k = 0 ; k < middle ; k++) {
+					cell += (arr1[i][k] * arr2[k][j])
+				}
+				row[j] = cell;
+			}
+			result[i] = row;
+		}
+
+		return result;
+	};
+
+	/* Scalar multiply */
+	let _scalar_mul = function(arr1, num){
+		if(!Array.isArray(arr1)) throw "MatrixException : first parameter is not array.";
+		else if(arr1.length == 0) return null;
+		else {
+			let result = [];
+			for(let i = 0 ; i < arr1.length ; i++) {
+				if(Array.isArray(arr1[i])) result[i] = _scalar_mul(arr1[i], num);
+				else result[i] = arr1[i] * num;
+			}
+			return result;
+		}
+	};
+
+	/* Evaluate function */
+	let _eval_mat = function(arr1, func) {
+		if(!Array.isArray(arr1)) throw "MatrixException : first parameter is not array.";
+		else if(typeof func != `function`) throw "MatrixException : second parameter is not function.";
+		else {
+			let result = [];
+			for(let i = 0 ; i < arr1.length ; i++) {
+				if(Array.isArray(arr1[i])) result[i] = _eval_mat(arr1[i], func);
+				else result[i] = func(arr1[i]);
+			}
+			return result;
+		}
 	}
 
 	/* Public methods */
@@ -43,7 +112,17 @@ const DlMatrix = function(ctxRoot){
 		return _matrix_shape(arr, undefined);
 	};
 	_root.DlMatrix.add = function(arr1, arr2){
-		return _matrix_add(arr1, arr2);
+		if(Array.isArray(arr2)) return _matrix_add(arr1, arr2);
+		else if(!isNaN(arr2)) return _scalar_add(arr1, arr2);
+		else null;
+	};
+	_root.DlMatrix.mul = function(arr1, arr2){
+		if(Array.isArray(arr2)) return _matrix_mul(arr1, arr2);
+		else if(!isNaN(arr2)) return _scalar_mul(arr1, arr2);
+		else null;
+	};
+	_root.DlMatrix.eval = function(arr, func){
+		return _eval_mat(arr, func);
 	};
 
 	/* Export */

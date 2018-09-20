42bda0b72f59b37de07944de621d92f5192a56f4
Pilmo Kang <ivorycirrus@gmail.com>
2018-09-20 12:15:48 +0900

    add module loader

---
+++ dl4vanilla.js
@@ -0,0 +1,38 @@
+const dl = (function(){
+	let _root = {};
+
+	const loadModule = function(moduleRoot, moduleName, modulePath, context) {
+		try {
+			if(typeof require !== `undefined`) {
+				moduleRoot[moduleName] = require(modulePath);
+			} else if(typeof window !== `undefined`) {
+				moduleRoot[moduleName] = window[modulePath](context)[modulePath];
+			} else {
+				throw "Cannot find Loader";
+			}
+		} catch(e) {
+			console.log("[Error] Cannot load module '"+moduleName+"'");
+		}
+	};
+
+	if(typeof require !== `undefined`) {
+		/* node.js module */
+		loadModule(_root, "mat", "./math/matrix");
+		loadModule(_root, "actv", "./nn/activation_function");
+		loadModule(_root, "loss", "./nn/loss_function");
+		loadModule(_root, "opt", "./nn/optimizer");
+	} else {
+		/* web browser */
+		loadModule(_root, "mat", "DlMatrix", this);
+		loadModule(_root, "actv", "DlActivationFunction", this);
+		loadModule(_root, "loss", "DlLossFunction", this);
+		loadModule(_root, "opt", "DlOptimizer", this);
+	}
+
+	return _root;
+})();
+
+/* Export for node.js */
+if(typeof module !== `undefined`) {
+	module.exports = dl;
+}

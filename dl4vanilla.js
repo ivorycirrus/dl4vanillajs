94fa8ff6d18160402e52a79a42224b5bac32cb3d
Pilmo Kang <ivorycirrus@gmail.com>
2018-09-26 14:04:06 +0900

    ModuleLoader : add DlDerivative

--- dl4vanilla.js
+++ dl4vanilla.js
@@ -18,12 +18,14 @@ const dl = (function(){
 	if(typeof require !== `undefined`) {
 		/* node.js module */
 		loadModule(_root, "mat", "./math/matrix");
+		loadModule(_root, "diff", "./math/derivative");
 		loadModule(_root, "actv", "./nn/activation_function");
 		loadModule(_root, "loss", "./nn/loss_function");
 		loadModule(_root, "opt", "./nn/optimizer");
 	} else {
 		/* web browser */
 		loadModule(_root, "mat", "DlMatrix", this);
+		loadModule(_root, "diff", "DlDerivative", this);
 		loadModule(_root, "actv", "DlActivationFunction", this);
 		loadModule(_root, "loss", "DlLossFunction", this);
 		loadModule(_root, "opt", "DlOptimizer", this);

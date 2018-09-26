fd691ae14c2e5a8c0160f4c24c881898a2a5a0b3
Pilmo Kang <ivorycirrus@gmail.com>
2018-09-26 21:42:29 +0900

    DlOptimizer : implement gradient_decent_optimizer

--- nn/optimizer.js
+++ nn/optimizer.js
@@ -3,15 +3,45 @@ const DlOptimizer = function(ctxRoot){
 	if(!_root.hasOwnProperty('DlOptimizer')) _root.DlOptimizer = {};
 
 	/* imports */
-	let mat;
+	let mat, diff;
 	if(typeof require === `undefined`){
 		mat = _root["DlMatrix"];
+		diff = _root["DlDerivative"];
 	} else {
 		mat = require("../math/matrix");
+		diff = require("../math/derivative");
 	}
 	if(!mat) throw "Import exception : DlMatrix module is not found. (DlOptimizer)";
+	else if(!diff) throw "Import exception : DlDerivative module is not found. (DlOptimizer)";
 
-	
+	/* Gradient Decent Optimizer */
+	let _gradient_decent_optimizer = function(f, x, lr=0.001){		
+		if(typeof f !== `function`) {
+			throw "OptimizerException : first parameter is not function";
+		} else if(!Array.isArray(x)) {
+			throw "OptimizerException : second parameter is not array";
+		}
+
+		let grad = diff.grad(f, x);
+		let learn = function(_x, _grad){
+			if(_x.length == 0) return null;
+			else {
+				let result = [];
+				for(let i = 0 ; i < _x.length ; i++) {
+					if(Array.isArray(_x[i])) result[i] = learn(_x[i], _grad[i]);
+					else result[i] = _x[i] - lr*_grad[i];
+				}
+				return result;
+			}
+		};
+
+		return learn(x, grad);
+	};
+
+	/* Public methods */
+	_root.DlOptimizer.gd = function(f, x, learn_rate){
+		return _gradient_decent_optimizer(f, x, learn_rate);
+	};
 
 	/* Export */
 	return _root;

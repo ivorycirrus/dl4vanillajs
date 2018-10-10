59689cf763ab7bbb9bc36e91458107f1c109768e
Pilmo Kang <ivorycirrus@gmail.com>
2018-10-10 19:28:58 +0900

    DlLossFunction : fix loss calculation for batch jobs

--- nn/loss_function.js
+++ nn/loss_function.js
@@ -11,82 +11,103 @@ const DlLossFunction = function(ctxRoot){
 	}
 	if(!mat) throw "Import exception : DlMatrix module is not found. (DlLossFunction)";
 
-	/* Mean Square Error = 0.5 * sum{k}((y_k - t_k)^2) */
-	let _mean_square_error = function(y, t, batch_size=1){		
+	/* Mean Square Error = (1/2n) * sum{k}((y_k - t_k)^2) */
+	let _mean_square_error = function(y, t){		
 		if(!Array.isArray(y) || !Array.isArray(t)) throw "MSEException : parameters are only alows array type.";
 
-		const shape_y = mat.shape(y);
-		const shape_t = mat.shape(t);
-		if(shape_y.reduce((p,n)=>p*n) != shape_t.reduce((p,n)=>p*n)) {
-			throw "MSEException : input arrays are not same size.";
+		const flat_y = mat.flat(y);
+		const flat_t = mat.flat(t);
+
+		let batch_size = 1; 
+		if(flat_t.length === 0 || flat_y.length%flat_t.length!== 0) {
+			throw "MSEException : input arrays have invalid shapes.";
+		} else {
+			batch_size = flat_y.length/flat_t.length;
 		}
 
-		const flat_y = (shape_y.length == 1)?y:mat.flat(y);
-		const flat_t = (shape_t.length == 1)?t:mat.flat(t);
-
-		let sum = 0;
-		for(let i = 0 ; i < flat_y.length ; i++) {
-			let err = flat_y[i] - flat_t[i];
-			sum += (err*err);
+		let batch_sum = 0;
+		for(let b = 0 ; b < batch_size ; b++){
+			let sum = 0;
+			for(let i = 0 ; i < flat_t.length ; i++) {
+				let err = flat_y[b*flat_t.length+i] - flat_t[i];
+				sum += (err*err);
+			}
+			batch_sum += (0.5*sum/flat_t.length);
 		}
 
-		return 0.5*sum/batch_size;
+		return batch_sum/batch_size;
 	};
 
 	/* 
 	Cross Entropy Error = -1.0 * sum{k}(t_k * log(y_k))
 	                    = -1.0 * sum{k}(t_k * log(y_k + h))  || h := small value to prevent -INF 
 	*/
-	let _cross_entropy_error = function(y, t, batch_size=1, h=0.0001){
+	let _cross_entropy_error = function(y, t, h){
 		if(!Array.isArray(y) || !Array.isArray(t)) throw "CEEException : parameters are only alows array type.";
+		else if(h<=0) throw "CEEException : safe-guard value 'h' must have positive value.";
 
-		const shape_y = mat.shape(y);
-		const shape_t = mat.shape(t);
-		if(shape_y.reduce((p,n)=>p*n) != shape_t.reduce((p,n)=>p*n)) {
-			throw "CEEException : input arrays are not same size.";
+		const flat_y = mat.flat(y);
+		const flat_t = mat.flat(t);
+
+		let batch_size = 1; 
+		if(flat_t.length === 0 || flat_y.length%flat_t.length!== 0) {
+			throw "CEEException : input arrays have invalid shapes.";
+		} else {
+			batch_size = flat_y.length/flat_t.length;
 		}
 
-		const flat_y = (shape_y.length == 1)?y:mat.flat(y);
-		const flat_t = (shape_t.length == 1)?t:mat.flat(t);
-
-		let sum = 0;
-		for(let i = 0 ; i < flat_y.length ; i++) {
-			sum += (flat_t[i] * Math.log(flat_y[i] + h));
+		let safe_y;
+		let batch_sum = 0;
+		for(let b = 0 ; b < batch_size ; b++){
+			let sum = 0;
+			for(let i = 0 ; i < flat_t.length ; i++) {
+				safe_y = Math.max(h, flat_y[b*flat_t.length+i]);
+				sum += (flat_t[i] * Math.log(safe_y));
+			}
+			batch_sum += (-1.0*sum);
 		}
 
-		return -1.0*sum/batch_size;
+		return batch_sum/batch_size;
 	};
 	
 	/* Cross Entropy with Logits */
-	let _cross_entropy_with_logits = function(y, t, batch_size=1, h=0.0001){
+	let _cross_entropy_with_logits = function(y, t, h){
 		if(!Array.isArray(y) || !Array.isArray(t)) throw "CEEException : parameters are only alows array type.";
+		else if(h<=0) throw "CEEException : safe-guard value 'h' must have positive value.";
 		
-		const shape_y = mat.shape(y);
-		const shape_t = mat.shape(t);
-		if(shape_y.reduce((p,n)=>p*n) != shape_t.reduce((p,n)=>p*n)) {
-			throw "CEEException : input arrays are not same size.";
+		const flat_y = mat.flat(y);
+		const flat_t = mat.flat(t);
+
+		let batch_size = 1; 
+		if(flat_t.length === 0 || flat_y.length%flat_t.length!== 0) {
+			throw "CEEException : input arrays have invalid shapes.";
+		} else {
+			batch_size = flat_y.length/flat_t.length;
 		}
 
-		const flat_y = (shape_y.length == 1)?y:mat.flat(y);
-		const flat_t = (shape_t.length == 1)?t:mat.flat(t);
-
-		let sum = 0;
-		for(let i = 0 ; i < flat_y.length ; i++) {
-			sum += (flat_t[i]*Math.log(flat_y[i]+h) + (1-flat_t[i])*Math.log(1-flat_y[i]+h));
+		let safe_y;
+		let batch_sum = 0;
+		for(let b = 0 ; b < batch_size ; b++){
+			let sum = 0;
+			for(let i = 0 ; i < flat_t.length ; i++) {
+				safe_y = Math.max(h, flat_y[b*flat_t.length+i]);
+				sum += (flat_t[i]*Math.log(safe_y) + (1-flat_t[i])*Math.log(1-safe_y));
+			}
+			batch_sum += (-1.0*sum);
 		}
 
-		return -1.0*sum/batch_size;
+		return batch_sum/batch_size;
 	};
 
 	/* Public methods */
-	_root.DlLossFunction.mean_square_error = function(y, t, batch_size){
+	_root.DlLossFunction.mean_square_error = function(y, t){
 		return _mean_square_error(y, t);
 	};
-	_root.DlLossFunction.cross_entropy_error = function(y, t, batch_size){
-		return _cross_entropy_error(y, t);
+	_root.DlLossFunction.cross_entropy_error = function(y, t, h=0.0000001){
+		return _cross_entropy_error(y, t, h);
 	};
-	_root.DlLossFunction.cross_entropy_with_logits = function(y, t, batch_size){
-		return _cross_entropy_with_logits(y, t);
+	_root.DlLossFunction.cross_entropy_with_logits = function(y, t, h=0.0000001){
+		return _cross_entropy_with_logits(y, t, h);
 	};
 
 	/* Export */
